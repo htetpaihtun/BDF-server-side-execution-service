726696f1a3194f58626447db6437d7013b48d270
htetpaihtun <htetpaihtun@gmail.com>
2023-06-22 11:51:13 +0630

    Added .env

--- main.go
+++ main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"log"
+	"bufio"
+	"strings"
+	"os"
 	"net/http"
 	"github.com/htetpaihtun/BDF-server-side-execution-service/containersController"
 )
 
 func main() {
+
+	// Load env variable 
+	err := loadEnvFromFile(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Create a new router
 	router := http.NewServeMux()
 
@@ -22,4 +32,43 @@ func main() {
 // Handler for the home route
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Service is up and running.. C:"))
+}
+
+func loadEnvFromFile(filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Ignore empty lines and comments
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			log.Printf("Invalid line in %s: %s", filename, line)
+			continue
+		}
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+
+		// Set the environment variable in the current process
+		err := os.Setenv(key, value)
+		if err != nil {
+			log.Printf("Failed to set environment variable: %s", key)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	return nil
 }
